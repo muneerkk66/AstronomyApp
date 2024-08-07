2a7e012a8dd11623f27e0d868c06d7b0d5850b5b
Muneer Kalakuzhiyil <muneerkk66@gmail.com>
2024-08-07 20:47:51 +0100

    Initial Commit

---
+++ AstronomyApp/AstronomyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyAppApp.swift
+//  AstronomyApp
+//
+//  Created by Muneer K K on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AstronomyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstronomyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AstronomyApp
+//
+//  Created by Muneer K K on 07/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
